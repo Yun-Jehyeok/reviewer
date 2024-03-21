42c921b3b0038bc01a6a230f9a49d74c5494ee04
ruthless96 <halonahaley@naver.com>
2024-03-21 16:07:07 +0900

    2024.03.21 - MyPage
    MyPage Navigation Update

--- client/apis/userApi.ts
+++ client/apis/userApi.ts
@@ -1,66 +1,66 @@
 import {
-  changePwIFC,
-  editUserIFC,
-  emailIFC,
-  paymentIFC,
-  phoneIFC,
-  signinIFC,
-  signupIFC,
-  userIFC,
-} from '@/interfaces/userIFC';
-import { Apis } from '@/utils/api';
-import { QueryFunction } from '@tanstack/react-query';
+    changePwIFC,
+    editUserIFC,
+    emailIFC,
+    paymentIFC,
+    phoneIFC,
+    signinIFC,
+    signupIFC,
+    userIFC,
+} from "@/interfaces/userIFC";
+import { Apis } from "@/utils/api";
+import { QueryFunction } from "@tanstack/react-query";
 
 export const signinApi = async (user: signinIFC) => {
-  return await Apis.post('/user/login', user);
+    return await Apis.post("/user/login", user);
 };
 
 export const signupApi = async (user: signupIFC) => {
-  return await Apis.post('/user/register', user);
+    return await Apis.post("/user/register", user);
 };
 
 export const authPhoneApi = async (data: phoneIFC) => {
-  return await Apis.post('/user/phone', data);
+    return await Apis.post("/user/phone", data);
 };
 
 export const withdrawalApi = async (userId: string) => {
-  return await Apis.delete(`/user/withdrawal/${userId}`);
+    return await Apis.delete(`/user/withdrawal/${userId}`);
 };
 
 export const authEmailApi = async (data: emailIFC) => {
-  return await Apis.post('/user/email', data);
+    return await Apis.post("/user/email", data);
 };
 
 export const changePwApi = async (data: changePwIFC) => {
-  return await Apis.put('/user/pw', data);
+    return await Apis.put("/user/pw", data);
 };
 
 export const editUserApi = async (data: editUserIFC) => {
-  return await Apis.put(`/user/${data.id}`, data);
+    return await Apis.put(`/user/${data.id}`, data);
 };
 
 export const paymentApi = async (data: paymentIFC) => {
-  return await Apis.put(`/user/payment/${data.id}`, data);
+    return await Apis.put(`/user/payment/${data.id}`, data);
 };
 
 export const getUserApi: QueryFunction<userIFC, [_1: string, any]> = async ({
-  queryKey
+    queryKey,
 }) => {
-  try {
-    const [_1, setUser] = queryKey;
-    
-    const token = localStorage.getItem('token');
-    const res = await Apis.get(`/user/${token}`, {
-      next: {
-        tags: ['user'],
-      },
-    });
-  
-    if (res.success) setUser(res.user);
-    else throw new Error('Failed to fetch data');
-    
-    return res.user;
-  } catch(err) {
-    console.error('get User APi Error >>>> ', err);
-  }
+    try {
+        const [_1, setUser] = queryKey;
+
+        const token = localStorage.getItem("token");
+        const res = await Apis.get(`/user/${token}`, {
+            next: {
+                tags: ["user"],
+            },
+        });
+
+        if (res.success) setUser(res.user);
+        else throw new Error("Failed to fetch data");
+
+        return res.user;
+    } catch (err) {
+        console.error("get User APi Error >>>> ", err);
+    }
 };
--- client/states/userStates.ts
+++ client/states/userStates.ts
@@ -1,36 +1,36 @@
-import { userIFC } from '@/interfaces/userIFC';
-import { atom } from 'recoil';
+import { userIFC } from "@/interfaces/userIFC";
+import { atom } from "recoil";
 
-import { recoilPersist } from 'recoil-persist';
+import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
 interface IUserState extends userIFC {
-  token: string;
+    token: string;
 }
 export const userState = atom<IUserState>({
-  key: 'userState',
-  default: {
-    _id: '',
-    reputation: 0,
-    register_date: '',
-    profile_img: '',
-    posts: [],
-    point: 0,
-    phone: '',
-    nickname: '',
-    name: '',
-    login_way: '',
-    lang: [],
-    isSubmit: false,
-    grade: '',
-    getApplications: [],
-    email: '',
-    applications: [],
-    token: '',
-    price: 0,
-    introduce: '',
-    oneLineIntroduce: '',
-  },
-  effects_UNSTABLE: [persistAtom],
+    key: "userState",
+    default: {
+        _id: "",
+        reputation: 0,
+        register_date: "",
+        profile_img: "",
+        posts: [],
+        point: 0,
+        phone: "",
+        nickname: "",
+        name: "",
+        login_way: "",
+        lang: [],
+        isSubmit: false,
+        grade: "",
+        getApplications: [],
+        email: "",
+        applications: [],
+        token: "",
+        price: 0,
+        introduce: "",
+        oneLineIntroduce: "",
+    },
+    effects_UNSTABLE: [persistAtom],
 });
