271383111e67d54d6ac3f87ce89f1dc97c730d05
Yun Jehyeok <dbswpgur2@naver.com>
2024-07-23 11:32:53 +0900

    일단 여기까지... 100%

---
+++ client/app/(useLayout)/(beforeLogin)/register/_component/PhoneAuth.tsx
@@ -0,0 +1,85 @@
+// Library
+import { useMutation } from "@tanstack/react-query";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+
+// Components
+import CButton from "@/components/common/CButton";
+import { phoneico } from "@/components/common/CIcons";
+import CInput from "@/components/common/CInput";
+
+// Hooks & Utils
+
+// Api
+import { authPhoneApi } from "@/apis/userApi";
+
+// Interface & States
+import { IError } from "@/interfaces/commonIFC";
+
+interface IProps {
+    phone: {
+        value: any;
+        onChange: (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => void;
+    };
+    setAuthNumResponse: Dispatch<SetStateAction<string>>;
+    setPhoneErr: Dispatch<SetStateAction<boolean>>;
+    setPhoneErrMsg: Dispatch<SetStateAction<string>>;
+    setAuthCheckErr: Dispatch<SetStateAction<boolean>>;
+    setShowAuth: Dispatch<SetStateAction<boolean>>;
+    phoneErr: boolean;
+    phoneErrMsg: string;
+}
+
+export default function PhoneAuth({ setAuthNumResponse, phone, setPhoneErr, setPhoneErrMsg, setAuthCheckErr, setShowAuth, phoneErr, phoneErrMsg }: IProps) {
+    const phoneMutation = useMutation({
+        mutationFn: authPhoneApi,
+        onMutate: (variable) => {
+            console.log("onMutate", variable);
+        },
+        onError: (error: IError, variable, context) => {
+            console.error("phoneAuthErr:::", error);
+        },
+        onSuccess: (data, variables, context) => {
+            console.log("phoneAuthSuccess", data, variables, context);
+            if (data.success) setAuthNumResponse(data.msg);
+        },
+        onSettled: () => {
+            console.log("phoneAuthEnd");
+        },
+    });
+
+    const handleAuth = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let phoneVal = phone.value;
+
+        if (phoneVal === "") {
+            setPhoneErr(true);
+            setPhoneErrMsg("휴대폰 번호를 입력해주세요.");
+        } else {
+            setAuthCheckErr(false);
+            setShowAuth(true);
+            setAuthNumResponse("00000000");
+
+            phoneMutation.mutate({ phone: phoneVal });
+        }
+    };
+
+    return (
+        <div>
+            <div className="w-full flex gap-4">
+                <div className="flex-1">
+                    <CInput {...phone} type="text" placeholder="휴대폰 번호를 입력해주세요." label="Phone" isErr={phoneErr}>
+                        {phoneico}
+                    </CInput>
+                </div>
+
+                <div className={`w-fit flex flex-col justify-end ${phoneErr && "relative -top-1"}`}>
+                    <CButton title="인증하기" onClick={handleAuth} />
+                </div>
+            </div>
+
+            {phoneErr && <div className="text-[#ea002c] text-xs mt-1 pl-4">{phoneErrMsg}</div>}
+        </div>
+    );
+}
